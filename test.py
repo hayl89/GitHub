4ddb8640397a3df12054324d6b09a0ed784c4f8a
In Hwa Kim <inhwa.kim89@gmail.com>
2018-03-25 12:12:11 +1100

    test script enhanced

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python3
 
 
-thetext=input("Enter your name\n")
+prompt  = "Enter a some text "
+thetext = input(prompt)
 print ("This is what you entered:")
 print (thetext)
 
 #this is commited to DEV on 11:55AM
 #Now I am making another change to commit newer version to DEV branch
+
