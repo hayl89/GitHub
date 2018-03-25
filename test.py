cd16b9a9fcf3192e77c7ff6eadded6246cb89828
In Hwa Kim <inhwa.kim89@gmail.com>
2018-03-25 11:56:01 +1100

    HK20180325_2
    Added comment

--- test.py
+++ test.py
@@ -4,3 +4,6 @@
 thetext=input("Enter your name\n")
 print ("This is what you entered:")
 print (thetext)
+
+#this is commited to DEV on 11:55AM
+#Now I am making another change to commit newer version to DEV branch
